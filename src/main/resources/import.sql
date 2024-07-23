424fe6fa2901b52c58df2b984b181d2b159f13db
Ulisses Batista Santana <santana.gru@gmail.com>
2024-07-23 19:42:11 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$HDY6WHx3YljY.Khb6Tmdu.zsToT/4VzMBsCPxWOxxY9kX.GrLJEci');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$HDY6WHx3YljY.Khb6Tmdu.zsToT/4VzMBsCPxWOxxY9kX.GrLJEci');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
